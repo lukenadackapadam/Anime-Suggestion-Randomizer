3e3b373f714d2c703ab7057fe8084ce4f5cdfcf7
luke <luke.nadackapadam@gmail.com>
2024-03-07 10:58:22 -0600

    Edit styles

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,36 @@ function App() {
 
   return (
     <>
-      <Card className="m-auto my-auto font-mono" horizontal>
+      <Card className="m-auto my-auto font-serif" horizontal>
         <img id="anime_img" src={content.data.images.jpg.image_url} alt="" />
         <h1 className="self-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {content.data.title}
         </h1>
         <p>
-          Genre:{" "}
+          <span className="font-bold">Genre:</span>{" "}
           {content.data.genres.map((genre) => (
             <div>{genre.name}</div>
           ))}
         </p>
-        <p>Type: {content.data.type}</p>
-        <p>Rating: {content.data.rating}</p>
-        <p>Airing Status: {content.data.aired.string}</p>
-        <p>Episodes: {content.data.episodes}</p>
-        <p>Synopsis: {content.data.synopsis}</p>
-        <p>Score: {content.data.score}</p>
+        <p>
+          <span className="font-bold">Type:</span> {content.data.type}
+        </p>
+        <p>
+          <span className="font-bold">Rating:</span> {content.data.rating}
+        </p>
+        <p>
+          <span className="font-bold">Airing Status:</span> {content.data.aired.string}
+        </p>
+        <p>
+          <span className="font-bold">Episodes:</span> {content.data.episodes}
+        </p>
+        <p>
+          <span className="font-bold">Synopsis:</span> {content.data.synopsis}
+        </p>
+        <p>
+          <span className="font-bold">Score:</span> {content.data.score}
+        </p>
+
         <Button color="blue" pill>
           <a href={content.data.url} target="_blank" rel="noopener noreferrer">
             Click for more info!
